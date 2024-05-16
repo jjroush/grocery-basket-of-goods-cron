8451a04f51bcb1c04d7202de118b7f12a56c9308
jjroush <27168277+jjroush@users.noreply.github.com>
2024-05-15 22:38:57 -0500

    swap products

--- basket-of-goods.js
+++ basket-of-goods.js
@@ -95,7 +95,7 @@ export const basketOfGoods = [
 		product: "Firehook Rosemary SS Crckrs",
 		localGroceryId: 2486911,
 		nationalGroceryId:
-			"simple-mills-almond-flour-cracker-fine-ground-sea-salt-snack-packs-4-9oz/-/A-89539328",
+			"simple-mills-sea-salt-4-25oz/-/A-53286867",
 	},
 	{
 		product: "B&J's Phish Food Ice Cream",
--- index-red-circle.js
+++ index-red-circle.js
@@ -3,31 +3,36 @@ import {basketOfGoods} from "./basket-of-goods.js";
 import {addProducts} from "./utils/data.js";
 
 const goodsWebsitesPromises = basketOfGoods.map(async (good) => {
-	const website = await fetch(`https://www.target.com/p/${good.nationalGroceryId}`)
-		.then((response) => response.text());
+	try {
+		const website = await fetch(`https://www.target.com/p/${good.nationalGroceryId}`)
+			.then((response) => response.text());
 
-	const {window} = new JSDOM(website, {
-		// Theoretically Target could execute Node.js inside the JSDOM env however it is unlikely.
-		runScripts: "dangerously",
-		beforeParse: (window) => {
-			window.scrollTo = () => {
-			};
+		const {window} = new JSDOM(website, {
+			// Theoretically Target could execute Node.js inside the JSDOM env however it is unlikely.
+			runScripts: "dangerously",
+			beforeParse: (window) => {
+				window.scrollTo = () => {
+				};
+			}
+		});
+
+		let price;
+
+		price = window.__TGT_DATA__.__PRELOADED_QUERIES__.queries.filter(query => query.at(0).at(0) === '@web/domain-product/get-pdp-v1').at(0).at(1).data.product.price.current_retail;
+
+		if (!price) {
+			price = window.__TGT_DATA__.__PRELOADED_QUERIES__.queries.filter(query => query.at(0).at(0) === '@web/domain-product/get-pdp-v1').at(0).at(1).data.product.price.current_retail_min;
 		}
-	});
 
-	let price;
-
-	price = window.__TGT_DATA__.__PRELOADED_QUERIES__.queries.filter(query => query.at(0).at(0) === '@web/domain-product/get-pdp-v1').at(0).at(1).data.product.price.current_retail;
-
-	if (!price) {
-		price = window.__TGT_DATA__.__PRELOADED_QUERIES__.queries.filter(query => query.at(0).at(0) === '@web/domain-product/get-pdp-v1').at(0).at(1).data.product.price.current_retail_min;
+		return {
+			productId: good.localGroceryId,
+			price,
+			description: good.product
+		};
+	} catch (e) {
+		console.log("Issue Fetching:", good);
+		throw new Error(e);
 	}
-
-	return {
-		productId: good.localGroceryId,
-		price,
-		description: good.product
-	};
 });
 
 const goods = await Promise.all(goodsWebsitesPromises);
